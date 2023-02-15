f7357ac145f61b00ee0a324b9421d16a1aaa9bc0
Hristo Papanov <hristopapanov13@gmail.com>
2023-02-15 16:07:35 +0200

    Added homework 2

--- Homework 1/Task1/src/Main.java
+++ Homework 1/Task1/src/Main.java
@@ -18,5 +18,47 @@ public class Main {
                 System.out.println(i);
             }
         }
+
+        String wordOne = "abc";
+        String wordTwo = "dafc";
+        System.out.println(SameStrings(wordOne, wordTwo));
+
+        int[] nums = {1, 3, 1, 2, 5, 2, 1, 3};
+        SortingArray(nums);
+        for(int i=0; i<nums.length; i++)
+        {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public static String SameStrings(String wordOne, String wordTwo){
+        String sameSt = "";
+
+        for(int i=0; i<wordOne.length(); i++)
+        {
+            for(int j=0; j<wordTwo.length(); j++)
+            {
+                if(wordOne.charAt(i) == wordTwo.charAt(j))
+                {
+                    sameSt += wordOne.charAt(i);
+                }
+            }
+        }
+        return  sameSt;
+    }
+
+    public static void SortingArray(int[] arr){
+        for(int i=0; i<arr.length; i++)
+        {
+            for (int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i] > arr[j])
+                {
+                    int a = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = a;
+                }
+            }
+        }
     }
 }
---
+++ Homework 2/Task1/src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task 1
+        int[] nums = {1,2,3,4};
+        nums = ReverseArray(nums);
+
+        for(int i=0; i<nums.length; i++)
+        {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+        //Task 2
+        int[] numbers = {2, -1, 8};
+        int[] reverseArray = SumArray(numbers);
+
+        for(int i=0; i<reverseArray.length; i++)
+        {
+            System.out.print(reverseArray[i] + " ");
+        }
+        System.out.println();
+
+        //Task 3
+        String word = "zab";
+        int count = 2;
+
+        System.out.println(EncryptedString(word, count));
+    }
+
+    public static int[] ReverseArray(int[] numbers){
+        int[] reverseNums = new int[numbers.length];
+
+        int counter = 0;
+        for(int i=numbers.length-1; i>=0; i--)
+        {
+            reverseNums[counter] = numbers[i];
+            counter++;
+        }
+        return reverseNums;
+    }
+
+    public static int[] SumArray(int[] numbers)
+    {
+        int[] sumArray = new int[numbers.length];
+
+        for(int i=0; i<numbers.length; i++)
+        {
+            int sum = 0;
+            for(int j=0; j<=i; j++)
+            {
+                sum+=numbers[j];
+            }
+            sumArray[i] = sum;
+        }
+        return sumArray;
+    }
+
+    public static String EncryptedString(String word, int count)
+    {
+        String output = "";
+
+        for(int i=0; i<word.length(); i++)
+        {
+            int index = word.charAt(i)+count;
+            if(index>122)
+            {
+                index-=26;
+            }
+            char letter = (char)(index);
+
+            output += letter;
+        }
+        return output;
+    }
+}
