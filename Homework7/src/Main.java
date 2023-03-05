ddb14ee52bcd91b4239f72f55f158322532d893c
Hristo Papanov <hristopapanov13@gmail.com>
2023-03-05 20:04:55 +0200

    lo

--- Homework7/src/Main.java
+++ Homework7/src/Main.java
@@ -17,7 +17,6 @@ public class Main {
         for (int i = 0; i < st.length(); i++) {
             if (st.charAt(i) != 'О' || st.charAt(i) != 'З' || st.charAt(i) != 'П') {
                 return false;
-                //fsurgfhkds
             }
             if (counter >= 3) {
                 return false;
